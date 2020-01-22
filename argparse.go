f7103cd82b2ede978aaca031323b228f151eae69
BigB00st <botzer.2002@gmail.com>
2020-01-22 17:15:27 +0200

    completed basic argparse

--- argparse.go
+++ argparse.go
@@ -6,8 +6,20 @@ import (
 
 func parseArgs() {
 
-	// cgroup
-	pflag.String("memory-limit", "1GB", "Byte must be a positive integer with measurement unit (MB, MiB, GB...)")
-	pflag.Bool("memory-swappiness", false, "Allow swappiness in container")
+	// genereic
+	pflag.String("name", "locker", "Name of container (used in hostname and more)")
+
+	// cgroups
+	pflag.String("memory-limit", "1GB", "RAM limit of container in bytes")
+	pflag.Bool("memory-swappiness", false, "Allow swappiness in container (boolean)")
+	pflag.Int("cpu-count", 1, "Number of cpu cores to use in container")
+
+	// network
+	pflag.String("network", "forwarding", "Type of network to use")
+
+	// security
+	pflag.String("seccomp", "seccomp_default.json", "Seccomp profile name")
+	pflag.String("apparmor", "not-implemented", "not-implemented")
+
 	pflag.Parse()
 }
