30d5fa77f77275ca5ecf0ca97e5b8aa753efa20a
Yuval Dahan <yuvaldahn@gmail.com>
2020-01-23 17:58:19 +0200

    now able to pass arguments to the program

--- container.go
+++ container.go
@@ -7,6 +7,7 @@ import (
 	"syscall"
 	"github.com/spf13/viper"
 	"github.com/spf13/pflag"
+	"strings"
 )
 
 // Usage: ./locker command args...
@@ -60,7 +61,7 @@ func parent() {
 
 // Child process, runs requested command
 func child() {
-	nonFlagArgs := pflag.Args()
+	nonFlagArgs := strings.Fields(pflag.Args()[0])
 	fmt.Printf("Running: %v\n", nonFlagArgs[0:])
 	
 	//command to run
