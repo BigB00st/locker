cc312ff6898fd751c869d4ef9eee709bcdabd4de
BigB00st <botzer.2002@gmail.com>
2020-04-08 15:47:34 +0300

    require root for ls command

--- cli/command/ls.go
+++ cli/command/ls.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/pkg/errors"
 	"gitlab.com/amit-yuval/locker/image"
@@ -9,9 +10,13 @@ import (
 
 // Ls lists local images
 func Ls(args []string) error {
+	if os.Geteuid() != 0 {
+		return errors.New("locker pull needs to be executed as root")
+	}
+
 	out, err := image.ListImages()
 	if err != nil {
-		return errors.Wrap(err, "couldn't listing images")
+		return errors.Wrap(err, "couldn't list images")
 	}
 	fmt.Print(out)
 	return nil
